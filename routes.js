a9335c31330cba1fd5f36251963bbbaf42916f1a
apg84 <kimboscg@gmail.com>
2015-08-26 19:44:42 +0100

    Add use strict

--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
+'use strict'
+
 module.exports = function(app){
 	var phonebook = require('./actions/phonebook');
 	app.get('/phonebook', phonebook.findAll);
     	app.get('/phonebook/:surname', phonebook.findBySurname);
     	app.post('/phonebook', phonebook.add);
-    	app.put('/phonebook/:surname', phonebook.update);
+    	app.put('/phonebook/:id', phonebook.update);
     	app.delete('/phonebook/:id', phonebook.delete);
 }
 
