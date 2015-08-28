f32352d6f5c0bce9076d450034d21799ad917af7
apg84 <kimboscg@gmail.com>
2015-08-28 15:39:37 +0100

    Add support for properties file. Add basic validation of the URI provided in the properties file

--- server.js
+++ server.js
@@ -3,15 +3,27 @@
 var express = require('express'),
 bodyParser = require('body-parser'),
 mongoose = require('mongoose'),
-fs = require('fs');
+properties = require('./properties.js');
 
-var mongoUri = 'mongodb://localhost/phonebook';
-mongoose.connect(mongoUri);
+var mongoUri;
+
+//We check the properties to ensure we are running in the correct mode,
+//also set the database to either the provided URI or the test one.
+
+if(properties.test)
+    mongoUri = 'mongodb://localhost/phonebooktest';
+else
+    mongoUri = properties.dbURI;
+
+//Very basic validation, just to ensure user did not forget to add the URI on the properties file
+mongoose.connect(mongoUri, function(err) {
+    if(mongoUri === undefined) throw new Error('DB\'s URI must be provided'); 
+});
+
 var db = mongoose.connection;
 db.on('error', function () {
   throw new Error('unable to connect to database at ' + mongoUri);
 });
-//console.log(db.getname());
 
 var app = express();
 
